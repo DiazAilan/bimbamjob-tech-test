ede368f73ad5879287827b214f5bfcd41a575f70
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2023-02-04 14:22:55 +0100

    chore: add linter & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { useState } from 'react';
 import './App.scss';
-import Field, { Mower, MowerMovement, MowerOrientation } from './components/Field';
+import Field, {
+  Mower,
+  MowerMovement,
+  MowerOrientation,
+} from './components/Field';
 import FileInput from './components/FileInput';
 
 function App(): JSX.Element {
-  const [field, setField] = useState({ width: 0, height: 0, mowers: [] as Mower[] });
+  const [field, setField] = useState({
+    width: 0,
+    height: 0,
+    mowers: [] as Mower[],
+  });
 
   const loadConfigFile = (config: string) => {
     const instructions = config.split('\n');
@@ -12,29 +20,38 @@ function App(): JSX.Element {
     const mowerInstructions = instructions
       .slice(1)
       .filter((instruction: string, index: number) => index % 2 === 0)
-      .map((instruction: string, index: number) => instruction + instructions[index * 2 + 2]);
+      .map(
+        (instruction: string, index: number) =>
+          instruction + instructions[index * 2 + 2],
+      );
     const mowers = mowerInstructions.map((mowerData: string) => {
       const [positionData, movementData] = mowerData.split(' ');
-      const position = { x: parseInt(positionData[0]), y: parseInt(positionData[1]) }
+      const position = {
+        x: parseInt(positionData[0]),
+        y: parseInt(positionData[1]),
+      };
       const orientation = movementData[0] as MowerOrientation;
       const movement = [...movementData.slice(1)] as MowerMovement[];
       return { position, movement, orientation };
-    })
+    });
     setField({
       width: parseInt(fieldInstructions[0]),
       height: parseInt(fieldInstructions[1]),
-      mowers
-    })
-  }
+      mowers,
+    });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        {field.width && field.height
-          ? <Field config={field} />
-          : <div>No file data loaded</div>
-        }
-        <FileInput onFileLoaded={(fileData: string) => loadConfigFile(fileData)} />
+        {field.width && field.height ? (
+          <Field config={field} />
+        ) : (
+          <div>No file data loaded</div>
+        )}
+        <FileInput
+          onFileLoaded={(fileData: string) => loadConfigFile(fileData)}
+        />
       </header>
     </div>
   );
